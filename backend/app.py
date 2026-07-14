60a0ec09019e4c4b3573790e3975a4f0d4c7cbc6
Ronak Dokania <ronakdokania676@gmail.com>
2026-07-14 16:15:42 +0530

    Fix CORS for Vercel frontend

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://ai-resume-analyzer-seven-mu.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
